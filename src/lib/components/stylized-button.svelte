753e4a773524d9e5300665e46d4deabe0bade847
EvolHeartrise <evol.heartrise@gmail.com>
2024-01-25 02:50:21 -0800

    Update button styling in stylized-button component

--- src/lib/components/stylized-button.svelte
+++ src/lib/components/stylized-button.svelte
@@ -7,11 +7,17 @@
 <style>
 	/* Remove built in button styling */
 	button {
-		background: none;
+		background: white;
+		width: min(400px, 80%);
 		border: none;
 		padding: 0;
-		font: inherit;
 		cursor: pointer;
-		outline: inherit;
+		font-size: 30px;
+		height: 100px;
+		font-weight: bold;
+		border-radius: 10px;
+		box-shadow: 4px 4px 14px rgba(0, 0, 0, 0.8);
+		border: 2px solid #eee;
+		color: #111;
 	}
 </style>
