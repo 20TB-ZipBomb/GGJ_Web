9542e66578f13c30691ed8fb199a30b20a505319
EvolHeartrise <evol.heartrise@gmail.com>
2024-01-25 02:49:50 -0800

    Add background image and adjust layout styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,28 @@
+<div>
+	<slot />
+</div>
+
 <style>
-	/* global selector set html/body background to darkmode */
 	:global(html) {
 		background: #0e1321;
+		margin: 0;
+		background-image: url('newspaper-ripped.jpg');
+		background-repeat: no-repeat;
+		background-attachment: fixed;
+		background-size: cover;
+		/* darken background image */
+	}
+
+	:global(body) {
+		margin: 0;
+	}
+
+	div {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		height: 100vh;
+		gap: 20px;
 	}
 </style>
