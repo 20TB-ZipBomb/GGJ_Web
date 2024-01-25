0d334f327045b3a072e1a9b1ccd547e9fab58c62
Hevano <evanohirabe@gmail.com>
2024-01-25 14:50:27 -0800

    Basic Salary Slider

---
+++ src/lib/components/salary-slider.svelte
@@ -0,0 +1,51 @@
+<script>
+  import { createEventDispatcher } from 'svelte';
+  let salary = 1;
+
+  const dispatch = createEventDispatcher();
+  function update() {
+		dispatch('salary', {
+			salary: salary
+		});
+	}
+    
+</script>
+
+<div class="slider-container">
+  <div class="input-container">
+    <input bind:value={salary} type="range" min="1" max="100" orient="vertical" on:change={update} />
+  </div>
+    <h1 class="salary-label">{salary}k </h1>
+</div>
+
+<style>
+  .slider-container {
+    height: 40%;
+    width: 80%;
+    display: flex;
+    align-items: center;
+  }
+
+  .salary-label {
+    text-align: center;
+    font-size: 50pt;
+    display: inline;
+    margin: auto;
+  }
+
+  input {
+    height: 100%;
+    left: 10px;
+    display: inline;
+    margin: auto;
+    appearance: slider-vertical;
+    display: inline;
+  }
+
+  .input-container {
+    width: 20%;
+    height: 100%;
+    display: inline;
+  }
+
+</style>
---
+++ src/routes/salary-slider/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+  import SalarySlider from "$lib/components/salary-slider.svelte";
+  import StylizedButton from "$lib/components/stylized-button.svelte";
+
+  let userToJudge = "Dan"
+  let jobTitle = "Plumber"
+</script>
+
+<h1>Based their experience, award {userToJudge} a salary for a {jobTitle}</h1>
+<SalarySlider on:salary={(e)=>{console.log(e.detail.salary)}}/>
+<StylizedButton text="Confirm" />
+
+<style>
+  h1 {
+    text-align: center;
+  }
+</style>
