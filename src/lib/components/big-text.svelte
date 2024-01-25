e857f16fe49171c5651facd7f24e3b37ebfd0e58
EvolHeartrise <evol.heartrise@gmail.com>
2024-01-25 02:50:11 -0800

    Refactor big-text component to use script tag and add text prop

--- src/lib/components/big-text.svelte
+++ src/lib/components/big-text.svelte
@@ -1,2 +1,16 @@
-<style lang="ts">
+<script lang="ts">
+	export let text: String = '';
+</script>
+
+<h1>{text}</h1>
+
+<style>
+	h1 {
+		width: 100%;
+		text-align: center;
+		font-size: 5em;
+		color: #eee;
+		text-shadow: 2px 2px 10px rgba(0, 0, 0, 0.5);
+		margin: 0;
+	}
 </style>
