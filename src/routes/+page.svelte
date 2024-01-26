808f24c75be11298989e0dabc845186843f36027
Hevano <evanohirabe@gmail.com>
2024-01-26 15:39:30 -0800

    Add job form, goto between test pages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
 	import BigText from '$lib/components/big-text.svelte';
 	import MyNameIs from '$lib/components/my-name-is.svelte';
 	import StylizedButton from '$lib/components/stylized-button.svelte';
@@ -8,4 +9,4 @@
 <MyNameIs />
 <h2>Room Code</h2>
 
-<StylizedButton text="Join Game" />
+<StylizedButton text="Join Game"/>
---
+++ src/routes/job-form/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+  import JobForm from "$lib/components/job-form.svelte";
+</script>
+
+<JobForm on:jobSubmited={(e)=>{console.log(e.detail.title)}}/>
