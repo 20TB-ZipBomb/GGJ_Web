1681adc8cd4f434fdd010312544ac844b27d14be
EvolHeartrise <evol.heartrise@gmail.com>
2024-01-26 15:59:24 -0800

    Add join game functionality and input fields

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,31 @@
 	import BigText from '$lib/components/big-text.svelte';
 	import MyNameIs from '$lib/components/my-name-is.svelte';
 	import StylizedButton from '$lib/components/stylized-button.svelte';
+	import { JobbersWebClient } from '$lib/websocket-client';
+	import { onMount } from 'svelte';
+
+	let name: string = '';
+	let roomCode: string = '';
+	let serverAddress: string = '';
+
+	let buttonDisabled: boolean = false;
+	$: buttonDisabled = name.length === 0 || roomCode.length === 0 || serverAddress.length === 0;
+
+	let jobberClient: JobbersWebClient;
+
+	function joinGame(): void {
+		jobberClient = new JobbersWebClient(serverAddress);
+	}
+
+	onMount(() => {
+		console.log('mounted');
+	});
 </script>
 
 <BigText text="Jobbers" />
-<MyNameIs />
+<MyNameIs bind:name />
 <h2>Room Code</h2>
-
-<StylizedButton text="Join Game" />
+<input type="text" bind:value={roomCode} />
+<h2>Server WebSocket Address</h2>
+<input type="text" bind:value={serverAddress} />
+<StylizedButton disabled={buttonDisabled} text="Join Game" on:click={joinGame} />
