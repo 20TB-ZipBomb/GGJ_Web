f25ceccde65fab286e1a27d0e6e3ad09defff8ec
EvolHeartrise <evol.heartrise@gmail.com>
2024-01-26 20:32:02 -0800

    format code with Prettier

--- src/lib/components/job.svelte
+++ src/lib/components/job.svelte
@@ -1,27 +1,24 @@
 <script>
-  import { createEventDispatcher } from "svelte";
-  export let title = "Job Title";
-  export let isSelected = false;
+	import { createEventDispatcher } from 'svelte';
+	export let title = 'Job Title';
+	export let isSelected = false;
 
-
-  const dispatch = createEventDispatcher();
-  function select(){
-    dispatch("jobSelected", {
-      title: title
-    })
-  }
+	const dispatch = createEventDispatcher();
+	function select() {
+		dispatch('jobSelected', {
+			title: title
+		});
+	}
 </script>
 
-<p on:click={select} style={(isSelected) ? "border: 2px solid #008000" : ""}>{title}</p>
+<p on:click={select} style={isSelected ? 'border: 2px solid #008000' : ''}>{title}</p>
 
 <style>
-
-  p {
-    background-color: white;
+	p {
+		background-color: white;
 		text-align: center;
 		font-size: 2em;
 		margin: 5%;
-    text-align: center;
-  }
-
+		text-align: center;
+	}
 </style>
