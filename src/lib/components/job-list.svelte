1d6d0e70f6538627ef3cdaa71693b9dd12c67c6d
EvolHeartrise <evol.heartrise@gmail.com>
2024-01-26 22:18:27 -0800

    simplify job-list component

--- src/lib/components/job-list.svelte
+++ src/lib/components/job-list.svelte
@@ -1,25 +1,21 @@
 <script lang="ts">
-	import { createEventDispatcher } from 'svelte';
 	import Job from './job.svelte';
 	import type { Card } from '$lib/websocket-client';
 
 	export let jobs: Card[] = [];
 	export let height = '80%';
-
-	let currentSelection = '';
-
-	const dispatch = createEventDispatcher();
-	function onJobSelect(e: any) {
-		currentSelection = e.detail.title;
-		dispatch('jobSelected', {
-			title: currentSelection
-		});
-	}
+	export let selectedJob: Card | null = null;
 </script>
 
 <div style={`height: ${height};`}>
-	{#each jobs as { job_text }, job}
-		<Job title={job_text} isSelected={currentSelection == job_text} on:jobSelected={onJobSelect} />
+	{#each jobs as job}
+		<Job
+			title={job.job_text}
+			isSelected={selectedJob?.job_text == job.job_text}
+			on:click={() => {
+				selectedJob = job;
+			}}
+		/>
 	{/each}
 </div>
 
