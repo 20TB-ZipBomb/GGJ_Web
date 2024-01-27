a7f261b01da5de33778fdf0a0be1947388a20360
EvolHeartrise <evol.heartrise@gmail.com>
2024-01-26 23:09:55 -0800

    format code with Prettier

--- src/routes/salary-slider/+page.svelte
+++ src/routes/salary-slider/+page.svelte
@@ -1,17 +1,21 @@
 <script>
-  import SalarySlider from "$lib/components/salary-slider.svelte";
-  import StylizedButton from "$lib/components/stylized-button.svelte";
+	import SalarySlider from '$lib/components/salary-slider.svelte';
+	import StylizedButton from '$lib/components/stylized-button.svelte';
 
-  let userToJudge = "Dan"
-  let jobTitle = "Plumber"
+	let userToJudge = 'Dan';
+	let jobTitle = 'Plumber';
 </script>
 
 <h1>Based their experience, award {userToJudge} a salary for a {jobTitle}</h1>
-<SalarySlider on:salaryChanged={(e)=>{console.log(e.detail.salary)}}/>
+<SalarySlider
+	on:salaryChanged={(e) => {
+		console.log(e.detail.salary);
+	}}
+/>
 <StylizedButton text="Confirm" />
 
 <style>
-  h1 {
-    text-align: center;
-  }
+	h1 {
+		text-align: center;
+	}
 </style>
