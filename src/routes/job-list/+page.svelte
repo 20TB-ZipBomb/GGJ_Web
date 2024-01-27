12bfd9aa2540b22a5d92300a6030076788b5837a
EvolHeartrise <evol.heartrise@gmail.com>
2024-01-26 22:15:28 -0800

    format code with Prettier

--- src/routes/job-list/+page.svelte
+++ src/routes/job-list/+page.svelte
@@ -1,11 +1,18 @@
 <script>
-  import JobList from "$lib/components/job-list.svelte"
-  import StylizedButton from "$lib/components/stylized-button.svelte";
-
-  let job = "";
+	import JobList from '$lib/components/job-list.svelte';
+	import StylizedButton from '$lib/components/stylized-button.svelte';
+	let job = '';
 </script>
 
-<JobList jobs={[{title: "Fisher"}, {title: "Lawyer"}, {title: "Mechanic"}, {title: "Dark overlord"}]} on:jobSelected={(e)=>{job = e.detail.title}}/>
-<StylizedButton text={(job && job.length) ? `Confirm ${job}` : "Pick a job" }/>
-
-
+<JobList
+	jobs={[
+		{ title: 'Fisher' },
+		{ title: 'Lawyer' },
+		{ title: 'Mechanic' },
+		{ title: 'Dark overlord' }
+	]}
+	on:jobSelected={(e) => {
+		job = e.detail.title;
+	}}
+/>
+<StylizedButton text={job && job.length ? `Confirm ${job}` : 'Pick a job'} />
