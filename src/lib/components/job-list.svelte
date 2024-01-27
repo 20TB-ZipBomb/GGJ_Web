3443b0e9a711aa7eebb62b8cece7b0a6740329c7
EvolHeartrise <evol.heartrise@gmail.com>
2024-01-26 20:26:25 -0800

    code format with Prettier

--- src/lib/components/job-list.svelte
+++ src/lib/components/job-list.svelte
@@ -1,31 +1,31 @@
 <script lang="ts">
-  import { createEventDispatcher } from "svelte";
-  import Job from "./job.svelte";
+	import { createEventDispatcher } from 'svelte';
+	import Job from './job.svelte';
 
-   export let jobs : any;
-   export let height = "80%";
+	export let jobs: any;
+	export let height = '80%';
 
-   let currentSelection = "";
+	let currentSelection = '';
 
-   const dispatch = createEventDispatcher();
-   function onJobSelect(e : any){
-    currentSelection = e.detail.title;
-    dispatch('jobSelected', {
+	const dispatch = createEventDispatcher();
+	function onJobSelect(e: any) {
+		currentSelection = e.detail.title;
+		dispatch('jobSelected', {
 			title: currentSelection
 		});
-   }
+	}
 </script>
 
 <div style={`height: ${height};`}>
-  {#each jobs as { title }, job}
-    <Job {title} isSelected={currentSelection == title } on:jobSelected={onJobSelect}/>
-  {/each}
+	{#each jobs as { title }, job}
+		<Job {title} isSelected={currentSelection == title} on:jobSelected={onJobSelect} />
+	{/each}
 </div>
 
 <style>
-  div {
-    display: flex;
-    flex-direction: column;
-    overflow-y: scroll;
-  }
+	div {
+		display: flex;
+		flex-direction: column;
+		overflow-y: scroll;
+	}
 </style>
