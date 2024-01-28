83fc95b178087d69f2259be37aa8caaaf46dfad7
EvolHeartrise <evol.heartrise@gmail.com>
2024-01-28 13:43:19 -0800

    create FlexContainer component for simple fade transitions

---
+++ src/lib/components/flex-container.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { fade } from 'svelte/transition';
+</script>
+
+<div in:fade={{ delay: 500, duration: 300 }} out:fade={{ duration: 300 }}>
+	<slot />
+</div>
+
+<style>
+	div {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		overflow-y: scroll;
+		gap: 20px;
+	}
+</style>
