3d2325f0ef0310b4a04294711c077e0ac3f78272
EvolHeartrise <evol.heartrise@gmail.com>
2024-01-28 02:39:10 -0800

    Add big text and stylized button to job form page

--- src/routes/job-form/+page.svelte
+++ src/routes/job-form/+page.svelte
@@ -1,5 +1,19 @@
-<script>
-  import JobForm from "$lib/components/job-form.svelte";
+<script lang="ts">
+	import BigText from '$lib/components/big-text.svelte';
+	import JobForm from '$lib/components/job-form.svelte';
+	import StylizedButton from '$lib/components/stylized-button.svelte';
+
+	let jobTextInput: HTMLInputElement;
+	let jobTextInputText: string = '';
 </script>
 
-<JobForm on:jobSubmited={(e)=>{console.log(e.detail.title)}}/>
+<BigText text="Create a job title" />
+<JobForm bind:input={jobTextInput} bind:text={jobTextInputText} />
+<StylizedButton
+	disabled={jobTextInputText.length === 0}
+	text="Submit"
+	on:click={() => {
+		jobTextInputText = '';
+		jobTextInput.focus();
+	}}
+/>
