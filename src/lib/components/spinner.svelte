98c166e979c096fb679929bcb0cebd1a66ebb1f0
EvolHeartrise <evol.heartrise@gmail.com>
2024-01-27 16:52:41 -0800

    format code with Prettier

--- src/lib/components/spinner.svelte
+++ src/lib/components/spinner.svelte
@@ -1,27 +1,27 @@
 <script>
-  import { SyncLoader } from 'svelte-loading-spinners';
+	import { SyncLoader } from 'svelte-loading-spinners';
 
-  export let message = "Waiting";
+	export let message = 'Waiting';
 </script>
 
 <div class="waiting-container">
-  <h1>{message}</h1>
-  <div class="spinner-wrapper"><SyncLoader/></div>
+	<h1>{message}</h1>
+	<div class="spinner-wrapper"><SyncLoader /></div>
 </div>
 
 <style>
-  .waiting-container {
-    display: flex;
-  }
+	.waiting-container {
+		display: flex;
+	}
 
-  h1 {
-    display: inline;
-    align-self: end;
-    font-size: 4em;
-  }
-  .spinner-wrapper {
-    display: inline;
-    align-self: end;
-    padding-bottom: 2.5em;
-  }
+	h1 {
+		display: inline;
+		align-self: end;
+		font-size: 4em;
+	}
+	.spinner-wrapper {
+		display: inline;
+		align-self: end;
+		padding-bottom: 2.5em;
+	}
 </style>
