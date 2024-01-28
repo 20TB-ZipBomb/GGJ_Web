67b3ce7b2db477c0758d4ff958780baf81bafee3
EvolHeartrise <evol.heartrise@gmail.com>
2024-01-28 13:43:33 -0800

    add FlexContainer for each state

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import BigText from '$lib/components/big-text.svelte';
+	import FlexContainer from '$lib/components/flex-container.svelte';
 	import JobForm from '$lib/components/job-form.svelte';
 	import JobList from '$lib/components/job-list.svelte';
 	import MyNameIs from '$lib/components/my-name-is.svelte';
@@ -53,87 +54,113 @@
 </script>
 
 {#if clientState == ClientState.MENU}
-	<BigText text="Octopations" fontSize="4em" />
-	<MyNameIs bind:name bind:roomCode />
-	<h2 style="margin: 0">Server WebSocket Address</h2>
-	<input type="text" bind:value={serverAddress} />
-	<StylizedButton disabled={menuButtonDisabled} text="Join Game" on:click={joinGame} />
+	<FlexContainer>
+		<BigText text="Octopations" fontSize="4em" />
+		<MyNameIs bind:name bind:roomCode />
+		<h2 style="margin: 0">Server WebSocket Address</h2>
+		<input type="text" bind:value={serverAddress} />
+		<StylizedButton disabled={menuButtonDisabled} text="Join Game" on:click={joinGame} />
+	</FlexContainer>
 {:else if clientState == ClientState.CONNECTING}
-	<Spinner message="Connecting" />
+	<FlexContainer>
+		<Spinner message="Connecting" />
+	</FlexContainer>
 {:else if clientState == ClientState.LOBBY}
-	<Spinner message="Waiting for the game to begin" />
+	<FlexContainer>
+		<Spinner message="Waiting for the game to begin" />
+	</FlexContainer>
 {:else if clientState == ClientState.JOB_CREATION}
-	<BigText text="Create Job Titles" fontSize="4.5em" />
-	<BigText text="{createdJobTexts.length + 1} of {jobsToMake}" fontSize="3em" />
-	<JobForm bind:input={jobTextInput} bind:text={jobTextInputText} />
-	<StylizedButton
-		disabled={jobTextInputText.length === 0 ||
-			createdJobTexts.includes(jobTextInputText.toUpperCase())}
-		text="Submit"
-		on:click={() => {
-			jobTextInput.focus();
-			jobberClient.createJob(jobTextInputText);
-			createdJobTexts.push(jobTextInputText.toUpperCase());
-			createdJobTexts = createdJobTexts; // Force reactivity
-			jobTextInputText = '';
-		}}
-	/>
+	<FlexContainer>
+		<BigText text="Create Job Titles" fontSize="4.5em" />
+		<BigText text="{createdJobTexts.length + 1} of {jobsToMake}" fontSize="3em" />
+		<JobForm bind:input={jobTextInput} bind:text={jobTextInputText} />
+		<StylizedButton
+			disabled={jobTextInputText.length === 0 ||
+				createdJobTexts.includes(jobTextInputText.toUpperCase())}
+			text="Submit"
+			on:click={() => {
+				jobTextInput.focus();
+				jobberClient.createJob(jobTextInputText);
+				createdJobTexts.push(jobTextInputText.toUpperCase());
+				createdJobTexts = createdJobTexts; // Force reactivity
+				jobTextInputText = '';
+			}}
+		/>
+	</FlexContainer>
 {:else if clientState == ClientState.JOB_CREATION_DONE}
-	<Spinner message="Waiting for others to make jobs" />
+	<FlexContainer>
+		<Spinner message="Waiting for others to make jobs" />
+	</FlexContainer>
 {:else if clientState == ClientState.JOB_PICKING}
-	<BigText text="Interviewing for" fontSize="3em" />
-	<h2
-		style="
+	<FlexContainer>
+		<BigText text="Interviewing for" fontSize="3em" />
+		<h2
+			style="
         font-family: 'Belgrano';
         font-size: 3em;
         margin: 0;
     "
-	>
-		{jobberClient.jobCard.job_text}
-	</h2>
-	<BigText text="Choose your qualification" fontSize="3em" />
-	<JobList jobs={jobberClient.cards} bind:selectedJob />
-	<StylizedButton
-		text={selectedJob ? 'Pick a job' : 'Confirm'}
-		disabled={selectedJob == null}
-		on:click={() => {
-			if (selectedJob == null) return;
-			jobberClient.sendCardData(selectedJob.card_id);
-			selectedJob = null;
-		}}
-	/>
-{:else if clientState == ClientState.JOB_PICKING_DONE}
-	<Spinner message="Waiting for others to pick jobs" />
-{:else if clientState == ClientState.INTERVIEWEE}
-	<BigText text="You are being interviewed" fontSize="4em" />
-{:else if clientState == ClientState.INTERVIEWER}
-	{#if jobCards.length == 0}
-		<BigText text="You have no job titles left to send!" fontSize="4em" />
-	{:else}
-		<BigText text="Send past experience" fontSize="4em" />
-		<JobList jobs={jobCards} bind:selectedJob />
+		>
+			{jobberClient.jobCard.job_text}
+		</h2>
+		<BigText text="Choose your qualification" fontSize="3em" />
+		<JobList jobs={jobberClient.cards} bind:selectedJob />
 		<StylizedButton
-			text={'Send Job Experience'}
+			text={selectedJob ? 'Pick a job' : 'Confirm'}
 			disabled={selectedJob == null}
 			on:click={() => {
 				if (selectedJob == null) return;
-				jobberClient.sendInterceptCardData(selectedJob.card_id);
-				jobCards = jobCards.filter((card) => card.card_id != selectedJob?.card_id);
+				jobberClient.sendCardData(selectedJob.card_id);
 				selectedJob = null;
 			}}
 		/>
+	</FlexContainer>
+{:else if clientState == ClientState.JOB_PICKING_DONE}
+	<FlexContainer>
+		<Spinner message="Waiting for others to pick jobs" />
+	</FlexContainer>
+{:else if clientState == ClientState.INTERVIEWEE}
+	<FlexContainer>
+		<BigText text="You are being interviewed" fontSize="4em" />
+	</FlexContainer>
+{:else if clientState == ClientState.INTERVIEWER}
+	{#if jobCards.length == 0}
+		<FlexContainer>
+			<BigText text="You have no job titles left to send!" fontSize="4em" />
+		</FlexContainer>
+	{:else}
+		<FlexContainer>
+			<BigText text="Send past experience" fontSize="4em" />
+			<JobList jobs={jobCards} bind:selectedJob />
+			<StylizedButton
+				text={'Send Job Experience'}
+				disabled={selectedJob == null}
+				on:click={() => {
+					if (selectedJob == null) return;
+					jobberClient.sendInterceptCardData(selectedJob.card_id);
+					jobCards = jobCards.filter((card) => card.card_id != selectedJob?.card_id);
+					selectedJob = null;
+				}}
+			/>
+		</FlexContainer>
 	{/if}
 {:else if clientState == ClientState.VOTING}
-	<BigText text="Award Salary" />
-	<SalarySlider bind:salaryCents />
-	<StylizedButton
-		text="Confirm"
-		on:click={() => {
-			jobberClient.sendScoreSubmission(salaryCents);
-		}}
-	/>
+	<FlexContainer>
+		<BigText text="Award Salary" />
+		<SalarySlider bind:salaryCents />
+		<StylizedButton
+			text="Confirm"
+			on:click={() => {
+				jobberClient.sendScoreSubmission(salaryCents);
+			}}
+		/>
+	</FlexContainer>
 {:else if clientState == ClientState.VOTING_DONE}
-	<Spinner message="Waiting for others to vote" />
+	<FlexContainer>
+		<Spinner message="Waiting for others to vote" />
+	</FlexContainer>
 {:else if clientState == ClientState.GAME_FINISHED}
-	<BigText text="All rounds finished. Look up at the screen!" />
+	<FlexContainer>
+		<BigText text="All rounds finished. Look up at the screen!" />
+	</FlexContainer>
 {/if}
