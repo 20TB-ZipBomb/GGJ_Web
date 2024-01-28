a302930b944e7d499f49c20a44d309890a151bed
EvolHeartrise <evol.heartrise@gmail.com>
2024-01-28 02:43:23 -0800

    add fontSize prop to big-text component

--- src/lib/components/big-text.svelte
+++ src/lib/components/big-text.svelte
@@ -1,15 +1,15 @@
 <script lang="ts">
 	export let text: String = '';
+	export let fontSize: String = '5em';
 </script>
 
-<h1>{text}</h1>
+<h1 style="font-size: {fontSize}">{text}</h1>
 
 <style>
 	h1 {
 		font-family: 'Chomsky';
 		width: calc(100% - 40px);
 		text-align: center;
-		font-size: 5em;
 		color: white;
 		text-shadow:
 			2px 2px 10px rgba(0, 0, 0, 1),
