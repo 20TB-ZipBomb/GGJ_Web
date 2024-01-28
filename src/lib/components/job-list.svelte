3f808a4363e5794081848517fe1409839a5ac193
EvolHeartrise <evol.heartrise@gmail.com>
2024-01-27 23:09:24 -0800

    changes to job-list visuals

--- src/lib/components/job-list.svelte
+++ src/lib/components/job-list.svelte
@@ -3,11 +3,10 @@
 	import type { Card } from '$lib/websocket-client';
 
 	export let jobs: Card[] = [];
-	export let height = '80%';
 	export let selectedJob: Card | null = null;
 </script>
 
-<div style={`height: ${height};`}>
+<div>
 	{#each jobs as job}
 		<Job
 			title={job.job_text}
@@ -23,6 +22,15 @@
 	div {
 		display: flex;
 		flex-direction: column;
+		align-items: center;
 		overflow-y: scroll;
+		gap: 20px;
+		width: min(80%, 400px);
+		max-height: calc(100vh - 400px);
+		border: 1px solid rgb(35, 35, 35);
+		background-color: rgba(0, 0, 0, 0.317);
+		border-radius: 20px;
+		padding-top: 20px;
+		padding-bottom: 20px;
 	}
 </style>
--- src/lib/components/job.svelte
+++ src/lib/components/job.svelte
@@ -5,18 +5,40 @@
 
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
-<p on:click class:isSelected>{title}</p>
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<div on:click class:isSelected>
+	<p>{title}</p>
+</div>
 
 <style>
-	p {
+	div {
+		display: flex;
+		justify-content: center;
+		align-items: center;
 		background-color: white;
-		text-align: center;
-		font-size: 2em;
-		margin: 5%;
-		text-align: center;
+		width: 90%;
+		box-shadow:
+			4px 4px 4px rgba(0, 0, 0, 0.5),
+			0px 0px 4px rgba(0, 0, 0, 0.5);
+		border-radius: 10px;
+		transition: background-color 0.2s ease-in-out;
 	}
 
-	p.isSelected {
-		border: 2px solid #008000;
+	div:hover {
+		cursor: pointer;
+	}
+
+	div.isSelected {
+		background-color: #ffd700;
+	}
+
+	p {
+		color: black;
+		font-family: 'Belgrano';
+		width: 90%;
+		text-align: center;
+		font-size: 20px;
+		text-align: center;
+		user-select: none;
 	}
 </style>
