e283ce008b32bb902ee61e9724088cc85e1aec36
EvolHeartrise <evol.heartrise@gmail.com>
2024-01-27 22:51:12 -0800

    update job-list preview page

--- src/routes/job-list/+page.svelte
+++ src/routes/job-list/+page.svelte
@@ -1,31 +1,34 @@
 <script lang="ts">
+	import BigText from '$lib/components/big-text.svelte';
 	import JobList from '$lib/components/job-list.svelte';
 	import StylizedButton from '$lib/components/stylized-button.svelte';
 	import type { Card } from '$lib/websocket-client';
 	let jobs: Card[] = [
 		{
-			card_id: 1,
+			card_id: '1',
 			job_text: 'Fisher'
 		},
 		{
-			card_id: 2,
+			card_id: '2',
 			job_text: 'Lawyer'
 		},
 		{
-			card_id: 3,
+			card_id: '3',
 			job_text: 'Mechanic'
 		},
 		{
-			card_id: 4,
+			card_id: '4',
 			job_text: 'Dark overlord'
+		},
+		{
+			card_id: '5',
+			job_text: 'Software Engineer'
 		}
 	];
 
 	let selectedJob: Card | null = null;
 </script>
 
+<BigText text="Pick a job" />
 <JobList bind:selectedJob {jobs} />
-<StylizedButton
-	disabled={selectedJob == null}
-	text={selectedJob ? `Confirm ${selectedJob.job_text}` : 'Pick a job'}
-/>
+<StylizedButton disabled={selectedJob == null} text={selectedJob ? `Confirm` : 'Pick a job'} />
